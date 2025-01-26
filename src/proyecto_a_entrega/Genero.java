fe89fa57c42415d27b2c3a694cfa5d6432c34dc1
tuerjfg@jhrfbgj.con <lamhour@gmail.com>
2025-01-27 00:09:18 +0100

    ntgek

---
+++ src/proyecto_a_entrega/Genero.java
@@ -0,0 +1,38 @@
+package proyecto_a_entrega;
+
+public class Genero {
+
+    public static final String ROCK = "Rock";
+    public static String getRock() {
+		return ROCK;
+	}
+	public static String getPop() {
+		return POP;
+	}
+	public static String getOpera() {
+		return OPERA;
+	}
+	public static String getJazz() {
+		return JAZZ;
+	}
+	public static String getClasica() {
+		return CLASICA;
+	}
+	public static String getDesconocido() {
+		return desconocido;
+	}
+	public static final String POP = "Pop";
+    public static final String OPERA = "Ópera";
+    public static final String JAZZ = "Jazz";
+    public static final String CLASICA = "Clásica";
+    public static final String desconocido =  "Desconocido";
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
